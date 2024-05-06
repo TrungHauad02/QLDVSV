0e994c8820ccc7cc93110ceb85f316f5e7402a9b
Le Quang Lam <work.lamsun@gmail.com>
2024-05-06 09:57:32 +0700

    csp not set header

---
+++ src/main/java/CSPFilters/ContentSecurityPolicyFilter.java
@@ -0,0 +1,30 @@
+package Filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "ContentSecurityPolicy", urlPatterns = {"/*"})
+public class ContentSecurityPolicyFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Content-Security-Policy", 
+        		"default-src 'self'; "
+        		+ "script-src 'self' 'unsafe-eval' 'unsafe-inline' 'unsafe-hashes' https://ajax.googleapis.com; "
+        		+ "style-src 'self' https://stackpath.bootstrapcdn.com 'unsafe-inline'; "
+        		+ "img-src 'self'; "
+        		+ "frame-src 'self'; connect-src 'self'; "
+        		+ "form-action 'self';");
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+    }
+}
+
